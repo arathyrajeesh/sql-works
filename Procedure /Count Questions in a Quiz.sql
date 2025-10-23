0e61e7e8bb01e66000e5805bb012fc2b9f27ff5b
Arathy <arathyrajeesh2@gmail.com>
2025-10-23 14:01:22 +0530

    quiz added

---
+++ Procedure /Count Questions in a Quiz.sql
@@ -0,0 +1,23 @@
+use procedurework;
+
+CREATE TABLE questions (
+question_id INT AUTO_INCREMENT PRIMARY KEY,
+quiz_id INT,
+question_text VARCHAR(255)
+);
+INSERT INTO questions (quiz_id,question_text) VALUES ('1','what is django'), ('23','what is python'), ('4','what is c');
+
+DELIMITER //
+CREATE PROCEDURE CountQuestions(
+    IN p_quiz_id INT
+)
+BEGIN
+    SELECT COUNT(*) AS TotalQuestions
+    FROM questions
+    WHERE quiz_id = p_quiz_id;
+END //
+
+DELIMITER ;
+
+ CALL CountQuestions(2);
+select * from questions;
