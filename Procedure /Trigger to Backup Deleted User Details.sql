ac8ed520ac5cb2096931912c51299a1d42d89e91
Arathy <arathyrajeesh2@gmail.com>
2025-10-24 10:41:53 +0530

    Trigger to Backup Deleted User Details

---
+++ Procedure /Trigger to Backup Deleted User Details.sql
@@ -0,0 +1,21 @@
+CREATE TABLE deleted_users (
+    user_id INT,
+    username VARCHAR(50),
+    email VARCHAR(100),
+    deleted_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+DELIMITER //
+
+CREATE TRIGGER after_user_delete
+AFTER DELETE ON user
+FOR EACH ROW
+BEGIN
+    INSERT INTO deleted_users (user_id, username, email)
+    VALUES (OLD.user_id, OLD.username, OLD.email);
+END //
+
+DELIMITER ;
+select * from user;
+DELETE FROM user WHERE user_id = 5; 
+
